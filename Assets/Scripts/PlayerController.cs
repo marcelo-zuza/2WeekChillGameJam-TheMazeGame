2ab3dae8a34942a5c5c4bcae54b0a7ca0f947ab4
Marcelo Zuza <falecomzuza@gmail.com>
2025-06-11 10:21:33 -0300

    player movement done

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Compression;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 6f;
+    [SerializeField] float mouseSensitivity = 100f;
+    [SerializeField] float gravity = -9.8f;
+    [SerializeField] public Transform playerCamera;
+
+    private CharacterController playerController;
+    private Vector3 velocity;
+    private float xRotation = 0f;
+    
+
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // player movement
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * xAxis + transform.forward * zAxis;
+        playerController.Move(move * speed * Time.deltaTime);
+
+        // gravity
+        if (playerController.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        velocity.y += gravity * Time.deltaTime;
+        playerController.Move(velocity * Time.deltaTime);
+        
+    }
+}
